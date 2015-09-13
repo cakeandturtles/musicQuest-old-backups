78293e608517388ba57f25c701d764c923e2b3f4
jake tamagotchi <jakeonaut@gmail.com>
2015-09-13 16:03:32 -0400

    dumping backups from email

---
+++ README.txt
@@ -0,0 +1,2 @@
+collection of various forms of a game that I was working on freshman year of
+college
---
+++ scraps/etcNotes/Unsaved_Document_1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Unsaved_Document_1{
+
+	public static void main(String args[]){
+		for(;;){
+			System.out.println("ALBUM: TONIIIIIGHT");
+		}
+	}
+}
